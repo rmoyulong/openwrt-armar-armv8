a27ae0859ece1c17c751352a84d4f722dfc9b88f
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-12 17:23:29 +0800

    openwrt库扩展插件源

--- update_before/openwrt.sh
+++ update_before/openwrt.sh
@@ -5,5 +5,5 @@ rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
 rm -rf package/network/services/hostapd
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/openwrt.tar.gz
+sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/openwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
