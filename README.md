22cf9af7b8a5b6aa14589ac546607f150233eb88
rmoyulong <mojialin99@qq.com>
2026-05-18 13:56:47 +0800

    修改README.md说明

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # openwrt-armar-armv8
-N1 和 章鱼瑆球 OPENWRT
+[![build](https://img.shields.io/github/actions/workflow/status/rmoyulong/openwrt-armar-armv8/openwrt--N1-Latest.yml)](https://github.com/rmoyulong/openwrt-armar-armv8/actions/workflows/openwrt--N1-Latest.yml) [![downloads](https://img.shields.io/github/downloads/rmoyulong/openwrt-armar-armv8/total)](https://github.com/rmoyulong/openwrt-armar-armv8/releases) [![downloads@latest](https://img.shields.io/github/downloads/rmoyulong/openwrt-armar-armv8/latest/total)](https://github.com/rmoyulong/openwrt-armar-armv8/releases/latest)<br>
+openwrt库: armar-armv8架构
+N1和章鱼瑆球OPENWRT
---
+++ update_before/setup.sh
@@ -0,0 +1,30 @@
+#公用函数
+source $GITHUB_WORKSPACE/update_before/functions.sh
+
+merge_package master https://github.com/coolsnowwolf/packages package lang/rust
+git clone https://github.com/stupidloud/helloworld package/helloworld
+
+rm -rf package/helloworld/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+
+rm -rf package/helloworld/shadowsocks-rust
+merge_package master https://github.com/kenzok8/small/ package shadowsocks-rust
+merge_package master https://github.com/kenzok8/small/ package sing-box
+merge_package master https://github.com/kenzok8/small/ package luci-app-ssr-plus
+
+rm -rf package/helloworld/xray-core
+git_sparse_clone main https://github.com/Openwrt-Passwall/openwrt-passwall-packages xray-core
+git_sparse_clone main https://github.com/Openwrt-Passwall/openwrt-passwall-packages geoview
+
+rm -rf feeds/luci/applications/luci-app-homeproxy
+git clone https://github.com/VIKINGYFY/homeproxy package/luci-app-homeproxy
+
+rm -rf feeds/luci/applications/luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+sed -i "s|ARMv8|RELEASES_TAGS_KEYWORD|g" package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+
+cd package
+$GITHUB_WORKSPACE/update_before/Packages.sh
+$GITHUB_WORKSPACE/update_before/Handles.sh
