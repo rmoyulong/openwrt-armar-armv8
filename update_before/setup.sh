14b7f253ba3c82ad3eb6dc0ef7a0dff1cbf68e03
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-19 14:11:39 +0800

    更新到最新插件

--- update_before/setup.sh
+++ update_before/setup.sh
@@ -4,6 +4,9 @@ source $GITHUB_WORKSPACE/update_before/functions.sh
 rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone https://github.com/VIKINGYFY/homeproxy package/luci-app-homeproxy
 
+rm -rf feeds/luci/applications/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+
 cd package
 $GITHUB_WORKSPACE/update_before/Packages.sh
 $GITHUB_WORKSPACE/update_before/Handles.sh
